a94b7cc3537d34293016eb801659e1a62b6c10e5
shyam369-b <nudrikraju396@gmail.com>
2025-04-02 14:31:14 +0530

    day13

---
+++ day 12/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Data from './ram';
+
+function App() {
+  return (
+    <div className="App">
+      <Data/>
+      <Data/>
+      <Data/>
+      <Data/>
+    </div>
+  );
+}
+
+export default App;
---
+++ day 12/src/ram.js
@@ -0,0 +1,10 @@
+function Data() {
+    return (
+        <div class="sub">   
+        <h1>Resume Builder: Craft Your Career Path</h1>
+        <p>Standing out in today's competitive job market starts with a stellar resume. Our intuitive resume builder takes the stress out of creating professional, ATS-friendly resumes. Choose from expertly designed templates, customize your content, and let our guided approach ensure your skills and experience shine. Whether you're starting your career or looking to advance, our tool helps you present your best self to prospective employers.</p>
+       </div>
+    );
+  }
+  
+  export default Data;
